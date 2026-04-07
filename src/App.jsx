9dec912c7c8e5b9f5b6e1a8ff53351975e128aae
DhoniPraneeth <dhonipraneeth23@gmail.com>
2026-04-07 15:23:15 +0530

    Color Picker exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import Button from "./Button";
-import Component from "./Component";
-import Details from "./Details";
-import List from "./List";
+import ColorPicker from "./ColorPicker";
 function App() {
-   const fruits=[
-        {id: 1,name: "apple",calories: 150},
-        {id: 2,name: "orange",calories: 50},
-        {id: 3,name: "banana",calories: 70},  
-        {id: 4,name: "coconut",calories: 80},  
-        {id: 5,name: "pineapple",calories: 60}];
-   const vegetables=[
-        {id: 1,name: "Green Chilli",calories: 150},
-        {id: 2,name: "Tomato",calories: 50},
-        {id: 3,name: "Onion",calories: 70},  
-        {id: 4,name: "Potato",calories: 80},  
-        {id: 5,name: "Carrot",calories: 60}];
    return ( 
-      <>    
-        <Component/>
-      </>
-
+      <ColorPicker/>
    );
 }
 export default App

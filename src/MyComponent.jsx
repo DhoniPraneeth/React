6a6f54259bb9873ba41d54ad6077c0af751d416f
DhoniPraneeth <dhonipraneeth23@gmail.com>
2026-04-09 16:17:24 +0530

    Application of useEffect: Window Height and Width

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,43 +1,21 @@
-/*
-useEffect()=React Hook that tells React to do some code when:
-            =>This Component Re-renders
-            => This Component mounts(adding content into the DOM)
-            =>When the state of a value changes
-    Syntax:useEffect(function,[dependencies])
-    1.usEffect(()=>{})          =>Runs after every re-render
-    2.useEffect(()=>{},())      =>Runs only on mount
-    3.useEffect(()=>{},[value]) =>Runs on mount+when value changes
-
-    USES:
-    #1. Event Listners
-    #2. DOM Manipulation
-    #3. Subscriptions (real-time updates)
-    #4. Fetching Data from an API
-    #5. Clean up when a Component unmount(Removing content from DOM)    
-        Syntax:useEffect(()=>{
-            return()=>{
-                //Some Clean up code 
-                }
-            });
-*/
 import React,{useState,useEffect} from 'react';
 function MyComponent(){
-    const [count,setCount]=useState(0);
-    const [color,setColor]=useState("blue");
-    const increment=()=>setCount(c=>c+1);
-    const decrement=()=>setCount(c=>c-1);
-    const toggle=()=>setColor(c=>c=="blue"?"yellow":"blue");
+    const [height,setHeight]=useState(window.innerHeight);
+    const [width,setWidth]=useState(window.innerWidth);
     useEffect(()=>{
-        console.log("Hello Guys");
-        document.title=`${count} : ${color  } `;
-    },[color,count]);
-    return(<>
-        <h3 style={{color}}>Count: {count}</h3>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <br></br>
-        <button onClick={toggle}>Toogle color</button>
-    </>)
+        window.addEventListener("resize",()=>{
+            console.log("Hello guys");
+            setHeight(window.innerHeight);
+            setWidth(window.innerWidth);
+        })
+    });
+    useEffect(()=>{
+        document.title=`${width} ${height}`;
+    },[width,height]);
+    return (<div>
+        <h4>Height of the Window: ${height}</h4>
+        <h4>Width of the Window: ${width}</h4>
+    </div>);
 }
 export default MyComponent;
 
